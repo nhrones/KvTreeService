c87a8315089ebff1191f68688f8957dce3ec4f78
NDH <nick@ndhrones.com>
2025-01-12 14:08:55 -0500

    update

--- utils.ts
+++ utils.ts
@@ -1,6 +1,7 @@
 
 /** load test data set */
 export async function loadSample() {
+   await clear()
    const db = await Deno.openKv()
    await db.set(["env", "cwd"], "./")
    await db.set(["env", "host"], "http://localhost")
@@ -8,10 +9,14 @@ export async function loadSample() {
    await db.set(["cfg", "target"], "./dist")
    await db.set(["cfg", "include"], "./src")
    await db.set(["cfg", "options"], { debug: true, useKv: true, dbFile: "./data/db.db" })
-   await db.set(["users", 1], { id: 1, first: "John", last: "Doe", age: 25, address: { street: '123 Main st.', city: 'Gotham', state: "CA", zip: 45927 } })
-   await db.set(["users", 2], { id: 2, first: "Jim", last: "Smith", age: 35, address: { street: '456 A st.', city: 'Fremont', state: "CA", zip: 45938 } })
-   await db.set(["users", 3], { id: 3, first: "Joe", last: "Smoe", age: 45, address: { street: '789 B st.', city: 'Hayward', state: "CA", zip: 45941 } })
-   db.close()
+   await db.set(["users", "general", 1], { id: 1, first: "John", last: "Doe", age: 25, address: { street: '123 Main st.', city: 'Gotham', state: "CA", zip: 45927 } })
+   await db.set(["users", "general", 2], { id: 2, first: "Jim", last: "Smith", age: 35, address: { street: '456 A st.', city: 'Fremont', state: "CA", zip: 45938 } })
+   await db.set(["users", "general", 3], { id: 3, first: "Joe", last: "Smoe", age: 45, address: { street: '789 B st.', city: 'Hayward', state: "CA", zip: 45941 } })
+   await db.set(["users", "admins", "super-user", 1], { id: 1, first: "Joe", last: "Smoe", age: 85})
+   await db.set(["users", "admins", "super-user", 2], { id: 2, first: "John", last: "Smith", age: 25})
+   await db.set(["users", "admins", "super-user", 3], { id: 3, first: "Tricky", last: "Nick", age: 52})
+   await db.set(["users", "admins", "admin", 1], { id: 1, first: "Admin", last: "Admin", age: 60})
+    db.close()
 }
 
 /** delete all rows from the db */
